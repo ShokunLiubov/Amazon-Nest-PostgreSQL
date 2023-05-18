4b7e0b7c53cf260f5807c3e4f1c37119a842b6ce
ShokunReactDev <tertyshnaya99love@gmail.com>
2023-05-18 14:15:32 +0300

    auth backend part

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { getJwtConfig } from './../config/jwt.config'
+import { PrismaService } from './../prisma.service'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy, PrismaService],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: getJwtConfig
+  })]
 })
 export class AuthModule {}
