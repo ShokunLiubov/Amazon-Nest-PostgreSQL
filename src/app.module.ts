2442c0a2ccfb49b06e00559afb4455c847386073
ShokunReactDev <tertyshnaya99love@gmail.com>
2023-05-24 16:46:19 +0300

    create endpoint with main stats

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { ReviewModule } from './review/review.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(),AuthModule, UserModule, ProductModule, CategoryModule, ReviewModule],
+  imports: [ConfigModule.forRoot(),AuthModule, UserModule, ProductModule, CategoryModule, ReviewModule, StatisticsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
---
+++ src/statistics/statistics.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from './../prisma.service'
+import { UserService } from './../user/user.service'
+
+@Injectable()
+export class StatisticsService {
+    constructor(
+        private prisma: PrismaService, 
+        private userService: UserService
+    ) {}
+
+    async getMain(userId: number) {
+        const user = await this.userService.byId(userId, {
+            orders: {
+                select: {
+                    items: true
+                }
+            },
+            reviews: true
+        })
+        
+        const totalAmount = await this.prisma.$queryRaw`SELECT SUM("Order_item".price * "Order_item".quantity) AS totalAmount FROM "Order" JOIN "Order_item" ON "Order".id = "Order_item"."order_id" WHERE "Order".user_id = ${userId};`
+
+        return [
+            {
+                name: 'Orders',
+                value: user.orders.length
+            },
+            {
+                name: 'Reviews',
+                value: user.reviews.length
+            },
+            {
+                name: 'Favorites',
+                value: user.favorites.length
+            },
+            {
+                name: 'Total amount',
+                value: totalAmount
+            }
+        ]
+
+    }
+}
